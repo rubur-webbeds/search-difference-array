42204624e7ce83564c82469c8cc78e865a1a8734
Ruben Bustos <ruben_b_r_@hotmail.com>
2019-07-09 00:23:06 +0200

    add basic solutions

---
+++ search-difference-array/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace search_difference_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+
+        private void Start()
+        {
+            var set = new int[] { 12, 9, 7, 5, 3, 2, 1 };
+            var resultSet = new List<dynamic>();
+            var diff = 2;
+            int result;
+            int ite = 0;
+
+            for (int i = 0; i < set.Length; i++)
+            {
+                for (int j = 0; j < set.Length; j++)
+                {
+                    ite++;
+                    result = set[i] - set[j];
+                    if (result == diff)
+                    {
+                        resultSet.Add(new { A = set[i], B = set[j] });
+                    }
+                }
+            }
+
+            Console.WriteLine(ite);
+            foreach (var pair in resultSet)
+            {
+                Console.WriteLine(pair);
+            }
+
+            ite = 0;
+            resultSet.Clear();
+
+            for (int i = 0; i < set.Length; i++)
+            {
+                for(int j = i; j < set.Length; j++)
+                {
+                    ite++;
+                    result = set[i] - set[j];
+                    if (Math.Abs(result) == diff)
+                    {
+                        resultSet.Add(new { A = set[i], B = set[j] });
+                    }
+                }
+            }
+
+            Console.WriteLine(ite);
+            foreach(var pair in resultSet)
+            {
+                Console.WriteLine(pair);
+            }
+        }
+    }
+}
